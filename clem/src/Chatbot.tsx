9324f8760b966bcfc688e3d1a950b84c40452596
Albie Baker-Smith <bakersmitha@gmail.com>
2021-09-17 19:50:35 +0100

    Calls OpenAI

--- clem/src/Chatbot.tsx
+++ clem/src/Chatbot.tsx
@@ -1,3 +1,4 @@
+import key from './key.txt';
 
 export interface ChatMessage {
   /** If undefined, message is loading */
@@ -77,6 +78,7 @@ export class MockChatbot implements Chatbot {
 }
 
 export class Gpt3Chatbot implements Chatbot {
+  private url = 'https://api.openai.com/v1/engines/davinci/completions';;
   private delayed = new Delayed(new History());
 
   initialMessage(onMessage: (history: ChatMessage[]) => void) {
@@ -84,10 +86,51 @@ export class Gpt3Chatbot implements Chatbot {
     addMessage({text: 'Hi, I\'m Clem, how can I help?', fromUser: false}, {delayMs: 500});
   }
 
-  respond(prompt: string, onMessage: (history: ChatMessage[]) => void) {
+  async respond(prompt: string, onMessage: (history: ChatMessage[]) => void) {
     const addMessage = this.delayed.messageCallback(onMessage);
-    addMessage({ text: prompt, fromUser: true });
-    addMessage({ fromUser: false }, { delayMs: 400 });
-    addMessage({ text: 'Response', fromUser: false }, { delayMs: 1400 });
+    await addMessage({ text: prompt, fromUser: true });
+    const hs = this.delayed.history.messages;
+
+    await addMessage({ fromUser: false }, { delayMs: 400 });
+    const resp = await this.gpt3(hs);
+    await addMessage({ text: resp, fromUser: false });
+  }
+
+  private async gpt3(hs: ChatMessage[]): Promise<string> {
+    const prompt = this.formattedHistory(hs);
+    const key = await this.openAiKey();
+    const resp = await fetch(this.url, {
+      method: 'POST',
+      headers: { 
+        'Content-Type': 'application/json',
+        'Authorization': `Bearer ${key}`,
+      },
+      body: JSON.stringify({ 
+        prompt: prompt,
+        max_tokens: 128,
+        temperature: 0.8,
+        stop: '---',
+      })
+    });
+    const json = await resp.json();
+    const text: string = json['choices'][0]['text'];
+    const splits = text.split('---');
+    return splits[splits.length-1];
+  }
+
+  private async openAiKey(): Promise<string> {
+    const r = await fetch(key);
+    return r.text();
+  }
+
+  private promptHeader(): string {
+    return '';
+  }
+
+  private formattedHistory(messages: ChatMessage[]): string {
+    return messages.flatMap((m) => [
+      `${m.fromUser ? 'You': 'AI'}: ${m.text}`,
+      ...(m.fromUser! ? ['\n'] : ['\n---\n'])
+    ]).join('');
   }
 }
